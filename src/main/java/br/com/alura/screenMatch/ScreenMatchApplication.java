ed93aa34d15335a0ca0563cedf3aa351d4769156
i-malur <marialuizafernandes383@gmail.com>
2025-01-09 17:05:57 -0300

    atualizações

--- src/main/java/br/com/alura/screenMatch/ScreenMatchApplication.java
+++ src/main/java/br/com/alura/screenMatch/ScreenMatchApplication.java
@@ -16,7 +16,6 @@ public class ScreenMatchApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		Principal principal = new Principal();
 		principal.exibeMenu();
-
 	}
 }
 
